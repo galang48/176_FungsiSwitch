b4ac3159901dc3a20a046b935323112631540f7c
galang48 <galangyudha1945@gmail.com>
2024-03-26 10:22:03 +0700

    menampilkan output

--- Switch/Switch.cpp
+++ Switch/Switch.cpp
@@ -20,5 +20,12 @@ int main()
     cin >> panjang;
     cout << "Masukkan lebar: ";
     cin >> lebar;
+
+    prosedurLuas(panjang, lebar);
+    cout << "Luas persegi panjang dengan prosedur: " << luas << endl;
+
+    cout << "Luas persegi panjang dengan fungsi: " << fungsiLuas(panjang, lebar) << endl;
+
+    return 0;
 }
 
